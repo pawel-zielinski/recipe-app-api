8d64b9d2de710d6252e7e27e6ce1c35f81ba64fe
pawel-zielinski <pawelzet00.pz@gmail.com>
2021-10-01 12:25:07 +0200

    Added recipe model

--- app/core/models.py
+++ app/core/models.py
@@ -66,3 +66,22 @@ class Ingredient(models.Model):
     def __str__(self):
         """Represent ingredient as string value."""
         return self.name
+
+
+class Recipe(models.Model):
+    """Recipe object."""
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=255)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+    ingredients = models.ManyToManyField('Ingredient')
+    tags = models.ManyToManyField('Tag')
+
+    def __str__(self):
+        """Represent recipe as string value."""
+        return self.title
