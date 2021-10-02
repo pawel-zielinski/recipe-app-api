70faff0639efe55ff84fe88541a6ac7b68107149
pawel-zielinski <pawelzet00.pz@gmail.com>
2021-10-02 20:37:15 +0200

    Travis CI PermissionError: [Errno 13] Permission denied Hopeful patch in settings.py + Dockerfile whitespace reduction

--- Dockerfile
+++ Dockerfile
@@ -10,7 +10,6 @@ RUN apk add --update --no-cache --virtual .tmp-build-deps \
 RUN pip install -r /requirements.txt
 RUN apk del .tmp-build-deps
 
-
 RUN mkdir /app
 WORKDIR /app
 COPY ./app /app
--- app/app/settings.py
+++ app/app/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = 'vol/web/media'
+MEDIA_ROOT = '/vol/web/media'
 STATIC_ROOT = '/vol/web/static'
 
 AUTH_USER_MODEL = 'core.User'
